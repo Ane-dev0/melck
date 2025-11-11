7d4fd390cf541caab4aee9608f59b2ecc2087798
Ane <anefv0@gmail.com>
2025-11-10 22:15:29 -0300

    Correção

--- routes/web.php
+++ routes/web.php
@@ -7,8 +7,4 @@ use App\Http\Controllers\PaginaController;
 
 Route::get('/', [PaginaController::class, 'inicio']);
 
-Route::get('dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 require __DIR__.'/settings.php';
